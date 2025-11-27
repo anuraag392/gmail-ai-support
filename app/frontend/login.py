bd0e8784e2d1e1bddb11ced453e2792876cd9079
anuraagdas392 <anurtube76@gmail.com>
2025-11-28 04:15:28 +0530

    login demo

---
+++ app/frontend/login.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    st.title("🔐 Login")
+
+    st.info("This is the login page. Google OAuth will be added here soon.")
+
+    # Temporary fake login button
+    if st.button("Login as Test User"):
+        st.session_state["user_id"] = "test_user_123"
+        st.success("You are logged in!")
+        st.experimental_rerun()
